0a07a4ad11d407c78ea62a193e270706a8bd1118
B-Oz <bettysoftwareng@gmail.com>
2022-08-16 02:23:23 +0300

    ass3

--- ass3.js
+++ ass3.js
@@ -1,2 +1,17 @@
 // ODEV3:Maasi asgari ucretten az olanlara %50 zam,fazla olanlara ise %10 zam yapmak istiyoruz.
 
+const money = 5500;
+let salary = +prompt("Enter your salary:");
+
+if (money >= salary) {
+  let newSalary = (salary * 1.5).toFixed(0);
+  console.log(
+    `We are making a raise because your salary is less than the minimum wage.Your new salary : ${newSalary}`
+  );
+}
+ else if (money < salary) {
+  let newSalary = (salary * 1.1).toFixed(0);
+  console.log(
+    `Good news! we made a salary increase.Your new salary : ${newSalary}`
+  );
+}
